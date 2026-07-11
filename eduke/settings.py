b4bae9270b4322b3f05fcd5ead4cc6bcb6c1edfe
Abhijith C S <abhijithcs200@gmail.com>
2026-07-11 23:24:02 +0530

    Update settings.py

--- eduke/settings.py
+++ eduke/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'  # Base URL for static files
+STATIC_URL = 'static/'  # Base URL for static files
 
 # Paths for static files
 STATICFILES_DIRS = [
